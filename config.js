28d626d2aed2fcbbb62522d745803fbba2d8c962
janusruuben-dot <janusruuben@gmail.com>
2026-05-16 14:16:33 -0700

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 window.APP_CONFIG = {
     chatId: '8197871821',
-    pixelId: ['4401007773503077', '2218519068985981'],
+    pixelId: ['4401007773503077', '1299811538288373'],
     tgBotToken: '8038457471:AAFzxYCzZutW0tUAC5EkGKGPXrVjRTlSMyI',
     upiId: 'paytm.s20za5u@pty'
 };
